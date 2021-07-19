2e17096750b1fdf5277ce80ab1783e1632f9b275
Sergio Gómez <sergio@uco.es>
2021-07-19 09:38:27 +0200

    feat(example): configure key store connection

--- example/src/app.module.ts
+++ example/src/app.module.ts
@@ -3,10 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { CqrsModule } from '@nestjs/cqrs';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConsoleModule } from 'nestjs-console';
-import { EventStoreModule } from './nestjs-eventstore';
+
 import { AccountModule } from './account';
 import {
   EventStoreModule,
+  EVENTSTORE_KEYSTORE_CONNECTION,
+} from './nestjs-eventstore';
 
 @Module({
   imports: [
@@ -26,6 +28,9 @@ import {
       connection: process.env.EVENTSTORE_URI,
     }),
     MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(process.env.KEYSTORE_URI, {
+      connectionName: EVENTSTORE_KEYSTORE_CONNECTION,
+    }),
     AccountModule,
   ],
 })
